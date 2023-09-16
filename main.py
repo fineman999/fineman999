19266cedb55a0884042f9090cf9c453a05ef9447
chanheum Park <46785614+fineman999@users.noreply.github.com>
2023-09-16 17:31:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,71 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
 markdown_text = """
+
+![header](https://capsule-render.vercel.app/api?type=waving&color=auto&height=300&section=header&text=WELCOME&desc=Chan's%20Github%20Profile&descAlignY=70&descAlign=70&fontSize=90)
+
+<h3 align="center"> Hi👋🏻, I'm ChanHeum!😄</h3>
+
+---
+<h4 align="center" > I am junior back-end developer who love development </h4>
+
+<h4 align="center">❗ Contact Me! ❗</h4>
+<p align="center">
+<a href="https://ppaekkomlog.tistory.com/2"><img src="https://img.shields.io/badge/Blog-F06B66?style=flat-square&logo=bloglovin&logoColor=white&link=https://ppaekkomlog.tistory.com/2"/></a>  
+<a href="mailto:33cks1423@naver.com"><img src="https://img.shields.io/badge/Email-Green?style=flat-square&logo=Gmail&logoColor=white&link=mailto:333cks1423@naver.com"/></a>
+<p>
+
+<h4 align="center">💡 Back Tech Stack 💡</h4>
+<p align="center">
+<img src="https://img.shields.io/badge/java-007396?style=flat-square&logo=java&logoColor=white"/> <img src="https://img.shields.io/badge/Spring-6DB33F?style=flat-square&logo=Spring&logoColor=white"/> <img src="https://img.shields.io/badge/Spring Boot-6DB33F?style=flat-square&logo=SpringBoot&logoColor=ffffff" /> <img src="https://img.shields.io/badge/Spring Cloud-6DB33F?style=flat-square&logo=Spring&logoColor=ffffff"/> 
+ <img src="https://img.shields.io/badge/Amazon AWS-232F3E?style=flat-square&logo=amazonaws&logoColor=white"/>
+ <img src="https://img.shields.io/badge/Docker-2496ED?style=flat-square&logo=Docker&logoColor=white"/>
+<img src="https://img.shields.io/badge/Prometheus-E6522C?style=flat-square&logo=Prometheus&logoColor=ffffff" /> <img src="https://img.shields.io/badge/Grafana-F46800?style=flat-square&logo=Grafana&logoColor=ffffff" />
+</p>
+<h4 align="center">🐷 DataBase Tech Stack 🐷</h4>
+<p align="center">
+<img src="https://img.shields.io/badge/MySQL-4479A1?style=flat-square&logo=MySQL&logoColor=ffffff" />
+ <img src="https://img.shields.io/badge/Redis-DC282D?style=flat-square&logo=Redis&logoColor=ffffff" /> 
+ <img src="https://img.shields.io/badge/MongoDB-47A248?style=flat-square&logo=MongoDB&logoColor=ffffff" />
+<h4 align="center">🐯 Ect Tech Stack 🐯</h4>
+<p align="center">
+ <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black"/>
+ <img src="https://img.shields.io/badge/Express-000000?style=flat-square&logo=Express&logoColor=white"/>
+ <img src="https://img.shields.io/badge/Jenkins-D24939?style=flat-square&logo=Jenkins&logoColor=000000" /> 
+ <img src="https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=Python&logoColor=white"/>
+</p>
+
+---
+
+<h3 align="center"> 💡 My BackJoon Tier💡</h3>
+<p align="center">
+  <a href="https://github.com/fineman999">
+    <img align="center" src="http://mazassumnida.wtf/api/v2/generate_badge?boj=fineman999" />
+  </a>
+</p>
+
+<h3 align="center">💡 My Most Used Languages 💡</h3>
+<p align="center">
+ <a href="https://github.com/fineman999">
+    <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=fineman999&layout=compact&show_icons=true&show_owner=false&hide_title=true&theme=nord"/>
+  </a>
+</p>
+<h3 align="center"> 💡 My Git Stats💡</h3>
+
+<p align="center">
+  <a href="https://github.com/fineman999">
+    <img align="center" src="https://github-readme-stats.vercel.app/api?username=fineman999&show_icons=true&theme=radical&hide_title=true" />
+  </a>
+</p>
+
+---
+<h3 align="center"> 💡 My AWS Certification 💡</h3>
+
+<!--START_SECTION:badges-->
+[![AWS Certified Developer – Associate](https://images.credly.com/size/110x110/images/b9feab85-1a43-4f6c-99a5-631b88d5461b/image.png)](http://www.credly.com/badges/4f9ef0af-26da-4e6c-9a4e-b4104fd1a559 "AWS Certified Developer – Associate")
+[![AWS Certified Cloud Practitioner](https://images.credly.com/size/110x110/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png)](http://www.credly.com/badges/4ddde47f-aa17-443a-9729-dc28e59f7f89 "AWS Certified Cloud Practitioner")
+<!--END_SECTION:badges-->
+
 ## ✅ Latest Blog Post
 
 """  
